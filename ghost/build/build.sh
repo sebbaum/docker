0d1f98590b62991112029c3bea722962fc60d253
Sebastian Baum <sebastian.baum@ion2s.com>
2015-04-21 19:17:25 +0200

    - prepared for docker-compose; changed volume folder on host

--- ghost/build/build.sh
+++ ghost/build/build.sh
@@ -7,5 +7,6 @@ cd ${BUILD_FOLDER}
 #./backup.sh
 sudo docker kill ${CONTAINER}
 sudo docker rm ${CONTAINER}
+#sudo docker-compose up
 sudo docker build -t ${CONTAINER} .
-sudo docker run --name ${CONTAINER} --restart=always -v ~/ghost/content/:/var/lib/ghost/ -p 9876:2368 -d ${IMAGE}
+sudo docker run --name ${CONTAINER} --restart=always -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d ${IMAGE}
