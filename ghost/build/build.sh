1126401107c05b973849407c22103cfbc0d07823
Sebastian Baum <sebastian.baum@ion2s.com>
2015-04-22 18:09:28 +0200

    - changed build for production deployment and clean up

--- ghost/build/build.sh
+++ ghost/build/build.sh
@@ -7,6 +7,5 @@ cd ${BUILD_FOLDER}
 #./backup.sh
 sudo docker kill ${CONTAINER}
 sudo docker rm ${CONTAINER}
-#sudo docker-compose up
 sudo docker build -t ${CONTAINER} .
-sudo docker run --name ${CONTAINER} --restart=always -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d ${IMAGE}
+sudo docker run --name ${CONTAINER} --restart=always -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d -e NODE_ENV=production ${IMAGE}
--- ghost/build/config.sh
+++ ghost/build/config.sh
@@ -2,6 +2,6 @@
 
 CONTAINER=sb-ghost-blog
 IMAGE=${CONTAINER}
-# BUILD_FOLDER=~/ghost-blog/build
-BUILD_FOLDER=.
+BUILD_FOLDER=~/ghost-blog/build
+#BUILD_FOLDER=.
 BACKUP_FOLDER=~/ghost-blog/backup/
