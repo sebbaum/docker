60c7005df4be193f57664e26cc5d52846406925a
Sebastian Baum <seb.baum@googlemail.com>
2015-08-22 21:40:46 +0200

    - optimized backup script; fixed build script

--- ghost/backup.sh
+++ ghost/backup.sh
@@ -2,11 +2,13 @@
 
 USER=$1
 URL=$2
-CONTENT_FOLDER=/home/${USER}/ghost-blog/content/
+BACKUP_FOLDER=ghost-blog/backups/
+CONTENT_FOLDER=ghost-blog/content/
 TIMESTAMP=`date "+%Y%m%d-%H%M"`
-BACKUP_FILE=blog-sebbaum_${TIMESTAMP}.tar.gz
+BACKUP_FILE=blog-backup_${TIMESTAMP}.tar.gz
 
 echo "Backuping ghost blog content folder..."
 ssh -t ${USER}@${URL} "tar cvfz ${BACKUP_FILE} ${CONTENT_FOLDER}"
-scp -r ${USER}@${URL}:/home/${USER}/${BACKUP_FILE} ./
+ssh -t ${USER}@${URL} "mv ${BACKUP_FILE} ${BACKUP_FOLDER}"
+scp -r ${USER}@${URL}:/home/${USER}/${BACKUP_FOLDER}/${BACKUP_FILE} ./
 exit 0
--- ghost/build/build.sh
+++ ghost/build/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-source config.sh
+source ./config.sh
 
 echo "Building ghost-blog..."
 cd ${BUILD_FOLDER}
-#./backup.sh
 sudo docker kill ${CONTAINER}
 sudo docker rm ${CONTAINER}
 sudo docker build -t ${CONTAINER} .
@@ -13,5 +12,5 @@ if [ ${PRODUCTION} -eq 1 ]
 then
   sudo docker run --name ${CONTAINER} --restart=always -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d -e NODE_ENV=production ${IMAGE}
 else
-  sudo docker run --name ${CONTAINER} -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d ${IMAGE}
+  sudo docker run --name ${CONTAINER} -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d -e NODE_ENV=production ${IMAGE}
 fi
