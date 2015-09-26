42c62c53cc83e0d7ca355229b116e2c343308a90
Sebastian Baum <seb.baum@googlemail.com>
2015-09-26 15:01:42 +0200

    - moved script in other repo

--- keycloak/buildContext/createCert.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOMAIN=$1
-openssl genrsa -des3 -passout pass:x -out ${DOMAIN}.pass.key 2048
-openssl rsa -passin pass:x -in ${DOMAIN}.pass.key -out ${DOMAIN}.key
-rm ${DOMAIN}.pass.key
-openssl req -new -key ${DOMAIN}.key -out ${DOMAIN}.csr
-openssl x509 -req -days 365 -in ${DOMAIN}.csr -signkey ${DOMAIN}.key -out ${DOMAIN}.crt
