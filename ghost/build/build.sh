18c9bbebd3b78907b00751dbe7e0bac9aa6dac1e
Sebastian Baum <seb.baum@googlemail.com>
2015-09-26 15:01:14 +0200

    - changed in order to hide blog behind reverse proxy

--- ghost/build/build.sh
+++ ghost/build/build.sh
@@ -10,7 +10,7 @@ sudo docker build -t ${CONTAINER} .
 
 if [ ${PRODUCTION} -eq 1 ]
 then
-  sudo docker run --name ${CONTAINER} --restart=always -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d -e NODE_ENV=production ${IMAGE}
+  sudo docker run --name ${CONTAINER} --restart=always -v ~/ghost-blog/content/:/var/lib/ghost/ -p 10.0.0.1:9876:2368 -d -e NODE_ENV=production ${IMAGE}
 else
   sudo docker run --name ${CONTAINER} -v ~/ghost-blog/content/:/var/lib/ghost/ -p 9876:2368 -d -e NODE_ENV=production ${IMAGE}
 fi
--- ghost/build/config.sh
+++ ghost/build/config.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-CONTAINER=sb-ghost-blog
-IMAGE=${CONTAINER}
-PRODUCTION=0
-
-if [ ${PRODUCTION} -eq 1 ]
-then
-  BUILD_FOLDER=~/ghost-blog/build
-else
-  BUILD_FOLDER=.
-fi
+CONTAINER=sb-ghost-blog_1
+IMAGE=sb-ghost-blog
+PRODUCTION=1
 
 BACKUP_FOLDER=~/ghost-blog/backup/
